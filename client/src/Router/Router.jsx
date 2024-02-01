e4ce7ebb226337c2f4525e4de4e49f817ed8f608
Thisura-240 <thisuraransika115@gmail.com>
2024-02-01 21:55:57 +0530

    login

--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -2,6 +2,7 @@ import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
+import Login from "../components/Login";
 
 
 
@@ -13,7 +14,16 @@ const router = createBrowserRouter([
         { path: "/", element:<Home></Home> },
         //{ path: "/about", element:<About></About> }
       ]
+      
     },
+       //thisura
+    {
+      path:"/login",element: <Login/>
+    }
+   //end
   ]);
+      
+      
+      
 
   export default router;
