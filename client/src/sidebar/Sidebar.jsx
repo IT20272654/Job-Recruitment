667ece4ca0105361d3831e7746006ba880e89d7b
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-02 23:01:39 +0530

    Created Sidebar.jsx

---
+++ client/src/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Location from './Location'
+import Salary from './Salary'
+
+const Sidebar = ({haddleChange, haddleClick}) => {
+  return (
+    <div className='space-y-5'>
+        <h3 className='text-lg font-bold mb-2'>Filters</h3>
+        <Location haddleChange={haddleChange}/>
+        <Salary haddleChange={haddleChange} haddleClick = {haddleClick}/>
+
+    </div>
+  )
+}
+
+export default Sidebar
