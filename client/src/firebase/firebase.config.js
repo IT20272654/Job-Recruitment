4525d2b61272a666c09e6aa2cfde30a7fd78b669
Thisura-240 <thisuraransika115@gmail.com>
2024-02-04 12:50:52 +0530

    login-authentication(firebase)

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMkE_haAbT-yvQXcdVh3IcAZSrU2BV0tc",
+  authDomain: "job-portal-demo-8f1af.firebaseapp.com",
+  projectId: "job-portal-demo-8f1af",
+  storageBucket: "job-portal-demo-8f1af.appspot.com",
+  messagingSenderId: "679088066397",
+  appId: "1:679088066397:web:6615784cedb0e473c322b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
