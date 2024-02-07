000665d56508a1caf3948d27bdb7a586556457d9
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-08 01:21:31 +0530

    Updated Home Page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import Card from '../components/Card';
 import Jobs from './Jobs';
 import Sidebar from '../sidebar/Sidebar';
+import SideBarRight from '../sidebar/SideBarRight';
+
 
 
 const Home = () => {
@@ -82,6 +84,7 @@ const Home = () => {
                 jobLocation.toLowerCase() === selected.toLowerCase() || 
                 parseInt(maxPrice) === parseInt(selected) ||    
                 salaryType.toLowerCase() === selected.toLowerCase() ||
+                experienceLevel.toLowerCase() === selected.toLowerCase() ||
                 employmentType.toLowerCase() === selected.toLowerCase()
             ))
                 console.log(filteredJobs);
@@ -108,9 +111,17 @@ const Home = () => {
                     <Sidebar haddleChange={haddleChange} haddleClick={haddleChange}/>
                 </div>
 
+               
+
+
+
                 {/* job side */}
                 <div className='col-span-2 bg-white p-4 rounded-sm'>
-
+                <div className='flex justify-start md:flex-row flex-col md:gap-0 gap-4'>
+            <div className='flex md:rounded-s-md rounded shadow-sm ring-2 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset  md:w-1/7 w-full'>
+                <input type="text" name="title" id="title" placeholder='What position are you looking for?' className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-black-400 focus:right-0 sm:text-sm sm:leading-6'onChange={haddleInputChange}
+                value={query}/>
+            </div></div>
                   {
                     isLoading ? (<p className='font-medium'>Loading...</p>) : result.length > 0 ? (<Jobs result={result}/>) :
                     <>
@@ -137,7 +148,9 @@ const Home = () => {
                 </div>
                 
                 {/* right side */}
-                <div className='bg-white p-4 rounded'>Right</div>
+                <div className='bg-white p-4 rounded'>
+                    <SideBarRight haddleChange={haddleChange} haddleClick={haddleChange}/>
+                </div>
             </div>
         </div>
     )
