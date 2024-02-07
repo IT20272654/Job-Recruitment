7b65136f6d2527520ba3b26d6fd1bd8a81c3a36f
Shyamaladikari99 <shayamaladikari@gmail.com>
2024-02-07 06:11:24 +0530

    Page logo created

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
 
+//import logo
+import logo from '../assets/logo.svg'; 
+/* logo didnt came , vedio time 12.58*/
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
+    const[isMenuOpen, setIsMenuOpen] = useState(false);
+    const[isSticky, setIsSticky] = useState(false);
 
+    //toggle Menubtn
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+      
+    }
+    
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 0) {
+            setIsSticky(true);
+          }
+        };
+
+    window.addEventListener("scroll", handleScroll);
+    },[])
+    
+        
+    return (
+      <header>
+         <nav>
+            {/* for large devices */}
+            <div>
+                {/* for logo */}
+                <div>
+                    <img src = {logo} alt=""/>
+                </div>
+            </div>
+         </nav>
+      </header>
     );
 };
 
