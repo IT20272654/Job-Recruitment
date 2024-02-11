f44be5464b13deb6f6abf421f08438a698188a4e
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-11 15:57:07 +0530

    Updated Banner jsx file

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -7,7 +7,7 @@ export const Banner = ({query,haddleInputChange}) => {
     <div className='bg-color'>
     <table align='center'>
     <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20 py-24'>
-        <h1 className='Heading'>Find your <span className='text-blue'>new job</span> today...</h1>
+        <h1 className='Heading'>Find your <span className='text-blue'>New Job</span> today...</h1>
         <p className='text-lg text-black/70 mb-8'>Thousands of jobs in the computer, engineering and technology sectors are waiting for you.</p>
 
     </div>
