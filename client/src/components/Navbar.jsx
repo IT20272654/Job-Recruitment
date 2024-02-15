ad4a7cd73c72418ee95799504656a96371ee7406
Shyamaladikari99 <shayamaladikari@gmail.com>
2024-02-15 15:40:32 +0530

    navigation bar completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 //import logo
 import logo from "../assets/logo.svg"; 
+// import menuicons from react icons
+import {HiMenu} from 'react-icons/hi'
 
 
 const Navbar = () => {
@@ -28,17 +30,31 @@ const Navbar = () => {
         
     return (
       <header>
-         <nav>
+         <nav className="py-4 md:px-12 px-4 bg-white">
             {/* for large devices */}
-            <div>
+            <div className="flex items-center justify-between">
                 {/* for logo */}
                 <div className="cursor-pointer">
                     <img src = {logo} alt="" className="h-10"/>
                 </div>
                 {/* menu items for large devices */}
-                <div>
-                  <a href="/n"></>13.25
+                <div className="lg:flex items-center gap-3 hideen text-body text-lg ">
+                  <a href="/" className="block py-2 px-4 cursor-pointer text-primary hover:text-gray-400">Home</a>
+                  <a href="/" className="block py-2 px-4 cursor-pointer  hover:text-gray-400">Our Skills</a>
+                  <a href="/" className="block py-2 px-4 cursor-pointer  hover:text-gray-400">About Us</a>
+                  <a href="/" className="block py-2 px-4 cursor-pointer  hover:text-gray-400">Portfolio</a>
+                  <a href="/" className="block py-2 px-4 cursor-pointer  hover:text-gray-400">Feedback</a>
+                 
                 </div>
+
+                {/* Contact Us btn */}
+                <div className="lg:block hidden">
+                  <button className="outlineBtn">Contact Us</button>
+                </div>
+                {/* menu btn for mobile devices */}
+                <button onClick={toggleMenu} className="lg:hidden tex-body text-3x1">
+                <HiMenu/>
+                </button>
             </div>
          </nav>
       </header>
