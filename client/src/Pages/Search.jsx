7598521c0661b1df746bbcd6fb0f39ac8a5eae1f
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-16 23:52:31 +0530

    Updated Search.jsx

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -103,8 +103,91 @@ const Search = () => {
 
 
     return (
-        
+            <div>
                 
+           
+
+
+<table align='center'>         
+<section id="hero">
+<div class="search_box">
+   <div class="search">
+     <div class= "select_area">
+       <i class="fas fa-map-marker-alt map_icon"></i>
+       <div class="text">Search</div>
+     </div>
+     
+     <div class="line"></div>
+     
+     <div class = "text_and-icon">
+       <input type="text" class="search_text" id="search_text" placeholder="Search by Company, Keyword or the designation .."onChange={haddleInputChange}
+                value={query}/> 
+            <i class="fas fa-search search_icon"></i>
+              </div>
+        </div> </div> 
+        </section>
+        </table><br/>
+
+
+
+
+{/* <table align='center'>
+        <ul class="hList">
+  <li>
+    <a href="#click" class="menu">
+      <h2 class="menu-title">animals</h2>
+      <ul class="menu-dropdown">
+        <li>cat</li>
+        <li>dog</li>
+        <li>horse</li>
+        <li>cow</li>
+        <li>pig</li>
+      </ul>
+    </a>
+  </li>
+  <li>
+    <a href="#click" class="menu">
+      <h2 class="menu-title menu-title_2nd">names</h2>
+      <ul class="menu-dropdown">
+        <li>Kevin</li>
+        <li>Jim</li>
+        <li>Andy</li>
+      </ul>
+    </a>
+  </li>
+  <li>
+    <a href="#click" class="menu">
+      <h2 class="menu-title menu-title_3rd">things</h2>
+      <ul class="menu-dropdown">
+        <li>bench</li>
+        <li>pizza</li>
+        <li>Honda CB125</li>
+        <li>space</li>
+        <li>black matter</li>
+        <li>apple</li>
+        <li>philodendron</li>
+        <li>liver</li>
+        <li>brass</li>
+      </ul>
+    </a>
+  </li>
+  <li>
+    <a href="#click" class="menu">
+      <h2 class="menu-title menu-title_4th">Movies</h2>
+      <ul class="menu-dropdown">
+        <li>Godzilla</li>
+        <li>Man on Wire</li>
+        <li>Spirited Away</li>
+        <li>Interstellar</li>
+      </ul>
+    </a>
+  </li>
+</ul>  
+</table> */}
+
+
+
+
             <div className='bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:24 px-4 py-12'>
                 {/* left side */}
                 <div className='bg-white p-4 rounded'>
@@ -118,9 +201,9 @@ const Search = () => {
                 {/* job side */}
                 <div className='col-span-2 bg-white p-4 rounded-sm'>
                 <div className='flex justify-start md:flex-row flex-col md:gap-0 gap-4'>
-                <div className='flex md:rounded-s-md rounded shadow-sm ring-2 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset  md:w-1/7 w-full'>
-                <input type="text" name="title" id="title" placeholder='What position are you looking for?' className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-black-400 focus:right-0 sm:text-sm sm:leading-6'onChange={haddleInputChange}
-                value={query}/>
+                <div className='flex md:rounded-s-md rounded shadow-sm ring-2 ring-inset ring-gray-400 focus-within:ring-2 focus-within:ring-inset  md:w-1/7 w-full'>
+                {/* <input type="text" name="title" id="title" placeholder='What position are you looking for?' className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-black-400 focus:right-0 sm:text-sm sm:leading-6'onChange={haddleInputChange}
+                value={query}/> */}
                  </div></div>
                   {
                     isLoading ? (<p className='font-medium'>Loading...</p>) : result.length > 0 ? (<Jobs result={result}/>) :
@@ -141,20 +224,25 @@ const Search = () => {
                         </div>
                     ) : ""
                   }     
-
-                        
-
-                
                 </div>
                 
                 {/* right side */}
                 <div className='bg-white p-4 rounded'>
                     <SideBarRight haddleChange={haddleChange} haddleClick={haddleChange}/>
                 </div>
-                
                 </div>
-                
-  
+
+                <footer class="sticky-footer">
+                <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Available Jobs</a></li>
+                  <li><a href="#">Post A Job</a></li>
+                  <li><a href="#">My Jobs</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+                </footer>
+                <br/><br/>
+                </div>
     )
 }
 export default Search
