5532ba63ad3937718f7448de534cfe6b25cec091
Shyamaladikari99 <shayamaladikari@gmail.com>
2024-02-22 16:58:51 +0530

    created .js file

---
+++ client/src/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBeIOOc_YRzWUbiq07MhOsD6wtHKFW0zPE",
+    authDomain: "contactus-c4f74.firebaseapp.com",
+    projectId: "contactus-c4f74",
+    storageBucket: "contactus-c4f74.appspot.com",
+    messagingSenderId: "980799137485",
+    appId: "1:980799137485:web:30e3770c8fe09ec2982b84"
+  };
+
+
+
+  export default firebaseConfig;
