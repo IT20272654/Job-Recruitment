386c020290f6676f9eaa763e90adbf6322a41e90
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-27 01:44:56 +0530

    Updated Side Bars

--- client/src/sidebar/SideBarRight.jsx
+++ client/src/sidebar/SideBarRight.jsx
@@ -6,7 +6,7 @@ import EmployeeType from './EmployeeType'
 const Sidebar = ({haddleChange, haddleClick}) => {
   return (
     <div className='space-y-5'>
-        <h3 className='text-lg font-bold mb-2'>Filters</h3>
+        {/* <h3 className='text-lg font-bold mb-2'>Filters</h3> */}
         <JobPostingData haddleChange={haddleChange} haddleClick = {haddleClick}/>
         <EmployeeType haddleChange={haddleChange} haddleClick = {haddleClick}/>
     </div>
--- client/src/sidebar/Sidebar.jsx
+++ client/src/sidebar/Sidebar.jsx
@@ -5,10 +5,13 @@ import Salary from './Salary'
 const Sidebar = ({haddleChange, haddleClick}) => {
   return (
     <div className='space-y-5'>
+        <div className=''>
         <h3 className='text-lg font-bold mb-2'>Filters</h3>
         <Location haddleChange={haddleChange}/>
-        {/* <Salary haddleChange={haddleChange} haddleClick = {haddleClick}/> */}
-
+        <Salary haddleChange={haddleChange} haddleClick = {haddleClick}/> 
+        
+        
+        </div>
     </div>
   )
 }
