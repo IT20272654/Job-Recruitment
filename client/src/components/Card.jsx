2a1052c30f9ac28be760a76bd9a7c7b2db8702a9
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-27 01:42:44 +0530

    Updated Card.jsx

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FiCalendar, FiClock, FiDollarSign, FiMapPin} from "react-icons/fi"
+import {FiCalendar, FiCamera, FiClock, FiDollarSign, FiMapPin} from "react-icons/fi"
 
 
  const Card = ({data}) => {
@@ -9,19 +9,23 @@ import {FiCalendar, FiClock, FiDollarSign, FiMapPin} from "react-icons/fi"
 
    <section className='card'>
         <Link to = {`/job/${_id}`} className='flex gap-4 flex-col sm:flex-row items-start'>
-            <img src={companyLogo} alt="" />
+            <img draggable="false" src={companyLogo} alt="" />
             <div>
-                <h4 className='text-primary mb-1'>{companyName}</h4>
-                <h3 className='text-lg font-semibold mb-2'>{jobTitle}</h3>
-
+            <br/>
+                <h3 className='cardHeading'>{jobTitle}</h3>
+                <h4 className='cardCompany'>{companyName}</h4>
+                <br/>
                 <div className='text-primary/70 text-base flex flex-wrap gap-2 mb-2'>
                     <span className='flex items-center gap-2'><FiMapPin/>{jobLocation}</span>
                     <span className='flex items-center gap-2'><FiClock/>{employmentType}</span>
                     <span className='flex items-center gap-2'><FiDollarSign/>{minPrice}-{maxPrice}</span>
                     <span className='flex items-center gap-2'><FiCalendar/>{postingDate}</span>
                 </div>
+                
+                <br/>
+                {/* <p className='text-base text-primary/'>{description}</p> */}
+                <button className="px-8 py-2 bg-primary text-xl text-white rounded hover:bg-body hover:text-white transition-all duration-300" >View More</button>
 
-                <p className='text-base text-primary/'>{description}</p>
 
             </div>
         </Link>
