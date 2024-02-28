2d4ebcbec771ca3154d07ebe55c3a57326d9d9c1
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-29 01:14:06 +0530

    Updated Sidebar.jsx

--- client/src/sidebar/Sidebar.jsx
+++ client/src/sidebar/Sidebar.jsx
@@ -5,13 +5,11 @@ import Salary from './Salary'
 const Sidebar = ({haddleChange, haddleClick}) => {
   return (
     <div className='space-y-5'>
-        <div className=''>
+    
         <h3 className='text-lg font-bold mb-2'>Filters</h3>
         <Location haddleChange={haddleChange}/>
         <Salary haddleChange={haddleChange} haddleClick = {haddleClick}/> 
         
-        
-        </div>
     </div>
   )
 }
