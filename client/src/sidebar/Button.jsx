156cdec7b9456d8fba132cac03dc1cff03cc1e50
Shavindu Rajapaksha <shavindurajapaksha@gmail.com>
2024-02-29 01:13:38 +0530

    Updated Button Page

--- client/src/sidebar/Button.jsx
+++ client/src/sidebar/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({onClickHaddler, value, tittle}) => {
   return (
-    <button onClick={onClickHaddler} value={value} className={`px-4 py border text-base hover:bg-blue hover:text-white`}>
+    <button onClick={onClickHaddler} value={value} className={`px-4 py text-base hover:text-blue`}>
         {tittle}
     </button>
   )
