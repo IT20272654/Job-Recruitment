75df985521ca5e846daa6fa432ae8aafe0538aa1
Thisura-240 <thisuraransika115@gmail.com>
2024-02-29 23:21:46 +0530

    update login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,34 +1,131 @@
-import React from 'react'
-import { GoogleAuthProvider,signInWithPopup } from "firebase/auth";
-import app from '../firebase/firebase.config';
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
 
-import { getAuth } from "firebase/auth";
+const title = "Login";
+const socialTitle = "Login With Your Google Account";
+const btnText = "Login Now";
 
-    const Login = () =>{
-    const auth = getAuth();
-    const googleProvider = new GoogleAuthProvider();
-    const handleLogin = () => {
-       signInWithPopup(auth, googleProvider).then((result) => {
-            
-          
+
+const Login = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  const { signUpWithGmail, login } = useContext(AuthContext);
+
+  // console.log(signUpWithGmail);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const from = location.state?.from?.pathname || "/";
+
+  // login with google
+  const handleRegister = () => {
+    signUpWithGmail()
+      .then((result) => {
         const user = result.user;
-        console.log(user)
-   
-             }).catch((error) => {
-            
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-          });
-    }
-    return (
-        <div className='h-screen w-full flex items-center justify-center'>
-            <button className='bg-blue px=8 py=2 text-white'onClick={handleLogin}>Login</button>
-        </div>
-    )
-}
+        navigate(from, { replace: true });
+      })
+      .catch((error) => console.log(error));
+  };
 
-export default Login
+  // login with email password
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    // console.log(email, password);
+    login(email, password)
+      .then((result) => {
+        // Signed in
+        const user = result.user;
+        console.log(user);
+        alert("Login successful!");
+        navigate(from, { replace: true });
+        // ...
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        setErrorMessage(<div className="Erormessage">Please provide valid email & password!</div>);
+      });
+  };
+
+  return (
+    
+    <table align="center">
+      <br/><br/>
+    <div className="loginform">
+      <table align="center">
+      <div className=" padding-tb section-bg">
+        <div className="container">
+          <div className="account-wrapper">
+            <h3 className="title">{title}</h3>
+
+            <form className="account-form" onSubmit={handleLogin}>
+              <div className="form-group">
+              <div  className="email">
+                <div>Email Address</div>
+                <input className="inputEmail"
+                  type="email"
+                  name="email"
+                  placeholder="Enter Email Address"
+                  required
+                />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="password">
+              <div>Password</div>
+                <input className="inputPassword"
+                  type="password"
+                  name="password"
+                  placeholder="Enter Password"
+                />
+                </div>
+              </div>
+              {/* showing error message */}
+              <div>
+                {errorMessage && (
+                  <div className="error-message text-danger">
+                    {errorMessage}
+                  </div>
+                )}
+              </div>
+              <div className="form-group">
+               
+              </div>
+              <div className="form-group text-center">
+                <button className="loginButton">
+                  <span>{btnText}</span>
+                </button>
+              </div>
+            </form>
+            <br/>
+            <div className="account-bottom">
+              <span > 
+                Don’t Have any Account? <Link to="/sign-up"><b><u>Sign Up</u></b></Link>
+              </span>
+              
+              <button onClick={handleRegister} className="github">
+                    <i className="icofont-github">Use single sign-on (Google) instead</i>
+                  </button>
+
+              {/* social icons */}
+              <table align="center">
+
+              <br/>   
+           
+            
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      </table>
+    </div>
+    </table>
+
+    
+  );
+};
+
+export default Login;
