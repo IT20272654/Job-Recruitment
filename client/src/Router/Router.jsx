109adbfb0568115f181aee64e2ef1aae96dff04f
Thisura-240 <thisuraransika115@gmail.com>
2024-03-08 10:37:25 +0530

    updating Errors

--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -1,8 +1,12 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
-import About from "../Pages/About";
-import Login from "../components/Login";
+import Login from "../components/Login.jsx";
+import Signup from "../components/Signup.jsx";
+import About from "../Pages/About.jsx";
+import PrivateRoute from "../PrivateRoute/PrivateRoute.jsx";
+
+/*import PrivateRoute from "../PrivateRoute/PrivateRoute.jsx";*/
 
 
 
@@ -12,18 +16,24 @@ const router = createBrowserRouter([
       element: <App/>,
       children: [
         { path: "/", element:<Home></Home> },
-        //{ path: "/about", element:<About></About> }
+       
       ]
-      
     },
-       //thisura
+
     {
-      path:"/login",element: <Login/>
+      path:"login",
+      element:<Login/>
+    },
+    {
+      path: "/sign-up",
+      element: <Signup/>
+    },
+    { path: "/About", 
+    element:<PrivateRoute><About/></PrivateRoute >
     }
-   //end
+
+
+   
   ]);
-      
-      
-      
 
   export default router;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/Router.jsx'
+import AuthProvider from './contexts/AuthProvider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <RouterProvider router={router}/>
+  </AuthProvider>
 )
